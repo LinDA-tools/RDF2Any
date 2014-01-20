46464136333fce41486693307b6d3745fa88190d
Judie Attard <judieattard@gmail.com>
2014-01-20 10:25:28 +0100

    adding linda files

---
+++ linda/src/main/java/de/unibonn/iai/eis/linds/converters/impl/Converter.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package de.unibonn.iai.eis.linds.converters.impl;
+
+import java.io.File;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.rdf.model.Model;
+
+/**
+ * @author jattard
+ *
+ */
+public interface Converter {
+	
+	 /**
+	  * This method should convert the rdf data.
+	  * @param Dataset to convert (may include multiple models)
+	  */
+	File convert(Dataset rdfDataset);
+
+}
