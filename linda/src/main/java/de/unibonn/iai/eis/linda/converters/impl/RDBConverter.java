cb4112fe709c3fbac997cba5d08704cc2d34cfb1
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-04-12 11:59:33 +0200

    escape ' characters for INSERT statements in SQL

--- linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/RDBConverter.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/RDBConverter.java
@@ -10,6 +10,7 @@ import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 
 import de.unibonn.iai.eis.linda.converters.Converter;
+import de.unibonn.iai.eis.linda.helper.RDBHelper;
 
 /**
  * @author gsingharoy
@@ -39,7 +40,7 @@ public class RDBConverter implements Converter{
 			QuerySolution row= rdfResults.next();
 			RDFNode subject= row.get("subject");
 			Literal label= row.getLiteral("label");
-			strResult = "INSERT INTO "+this.tableName+" VALUES("+counter.toString()+", '"+subject.toString()+"', '"+label.toString()+"');\n";
+			strResult = "INSERT INTO "+this.tableName+" VALUES("+counter.toString()+", '"+RDBHelper.getSQLReadyEntry(subject.toString())+"', '"+RDBHelper.getSQLReadyEntry(label.toString())+"');\n";
 			output.write(strResult.getBytes(Charset.forName("UTF-8")));
 			counter++;
 		}
---
+++ linda/src/main/java/de/unibonn/iai/eis/linda/helper/RDBHelper.java
@@ -0,0 +1,28 @@
+package de.unibonn.iai.eis.linda.helper;
+
+
+/**
+ * @author gsingharoy
+ *
+ *This class handles RDB functionality
+ **/
+
+
+public class RDBHelper {
+
+	//This function returns a string which can be used in the INSERT
+	//statment of an SQL query
+	public static String getSQLReadyEntry(String str){
+		String resultString = "";
+		for(int i=0;i<str.length();i++){
+			if((int)str.charAt(i) == 39){
+				//Check for single quotes '
+				resultString +="'+char(39)+'";
+			}
+			else{
+				resultString += str.charAt(i);
+			}
+		}
+		return resultString;
+	}
+}
