9c248a7be6f01aa0fad0959cdcba90d224b52b70
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-04-13 18:54:38 +0200

    Completed JSON output

--- linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/JSONConverter.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/JSONConverter.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 
 import de.unibonn.iai.eis.linda.converters.impl.results.JSONOutput;
 
@@ -29,7 +31,27 @@ public class JSONConverter extends MainConverter {
 		Map<String, Map<String,String>> bindingEntry = new HashMap<String, Map<String,String>>();
 		for(int i=0;i<resultVars.size();i++){
 			Map<String,String> columnEntry = new HashMap<String,String>();
-			columnEntry.put("value", row.get(resultVars.get(i)).toString());
+			if(row.get(resultVars.get(i)) instanceof Literal ){
+				RDFNode literal = row.get(resultVars.get(i));
+				columnEntry.put("type", "literal");
+				if(literal.toString().length() > 3){
+					Integer languageIdentifierPoint = literal.toString().length()-3;
+					if(literal.toString().charAt(languageIdentifierPoint) == '@'){
+						columnEntry.put("xml:lang", literal.toString().substring(languageIdentifierPoint+1));
+						columnEntry.put("value", literal.toString().substring(0,languageIdentifierPoint));
+					}
+
+				}
+				else{
+					columnEntry.put("value", literal.toString());
+
+				}
+			}
+			else if(row.get(resultVars.get(i)) instanceof RDFNode ){
+				RDFNode node = row.get(resultVars.get(i));
+				columnEntry.put("type", "uri");
+				columnEntry.put("value", node.toString());
+			}
 			bindingEntry.put(resultVars.get(i), columnEntry);
 		}
 		this.jsonOutput.results.bindings.add(bindingEntry);
