61d86078b267be3823ac7409fcab4ddf8a00cb3a
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-04-13 18:35:28 +0200

    some modifications to JSONConverter

--- linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/JSONConverter.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/JSONConverter.java
@@ -1,5 +1,8 @@
 package de.unibonn.iai.eis.linda.converters.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 
@@ -22,10 +25,19 @@ public class JSONConverter extends MainConverter {
 		this.jsonOutput = new JSONOutput(rdfResultSet);
 		convert();
 	}
+	public void addResultSetRowToOutput(QuerySolution row){
+		Map<String, Map<String,String>> bindingEntry = new HashMap<String, Map<String,String>>();
+		for(int i=0;i<resultVars.size();i++){
+			Map<String,String> columnEntry = new HashMap<String,String>();
+			columnEntry.put("value", row.get(resultVars.get(i)).toString());
+			bindingEntry.put(resultVars.get(i), columnEntry);
+		}
+		this.jsonOutput.results.bindings.add(bindingEntry);
+	}
 	public void convert(){
 		while(rdfResults.hasNext()){
 			QuerySolution row= rdfResults.next();
-			jsonOutput.results.addBindingEntry(row, resultVars);
+			addResultSetRowToOutput(row);
 		}
 	}
 }
--- linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/results/JSONResult.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/converters/impl/results/JSONResult.java
@@ -1,11 +1,11 @@
 package de.unibonn.iai.eis.linda.converters.impl.results;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 
-import com.hp.hpl.jena.query.QuerySolution;
+
 
 
 /**
@@ -22,16 +22,6 @@ public class JSONResult {
 		this.distict = false;
 		this.ordered = true;
 		this.bindings = new ArrayList();
-
-	}
-	public void addBindingEntry(QuerySolution row, List<String> resultVars){
-		Map<String, Map<String,String>> bindingEntry = new HashMap<String, Map<String,String>>();
-		for(int i=0;i<resultVars.size();i++){
-			Map<String,String> columnEntry = new HashMap<String,String>();
-			columnEntry.put("value", row.get(resultVars.get(i)).toString());
-			bindingEntry.put(resultVars.get(i), columnEntry);
-		}
-		this.bindings.add(bindingEntry);
 
 	}
 }
