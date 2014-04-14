3e4ef001bc1d32ebf99e9a25e6b7a07c5080e9be
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-04-14 13:25:28 +0200

    added JSON rest API path for generic queries

--- linda/src/main/java/com/servlet/routes/ConverterRoute.java
+++ linda/src/main/java/com/servlet/routes/ConverterRoute.java
@@ -6,20 +6,25 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.core.UriInfo;
 
 import de.unibonn.iai.eis.linda.converters.impl.CSVConverter;
+import de.unibonn.iai.eis.linda.converters.impl.JSONConverter;
 import de.unibonn.iai.eis.linda.converters.impl.RDBConverter;
+import de.unibonn.iai.eis.linda.converters.impl.results.JSONOutput;
+import de.unibonn.iai.eis.linda.example.SPARQLExample;
 import de.unibonn.iai.eis.linda.helper.OutputStreamHandler;
+import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
 
 @Path("/v1.0/convert/")
 public class ConverterRoute {
 	@GET
 	@Path("csv-converter.csv")
 	@Produces({"application/csv"})
-	public StreamingOutput getCSVExample(@Context UriInfo uriInfo) {
+	public StreamingOutput getCSVConverter(@Context UriInfo uriInfo) {
 		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters(); 
 		String query = queryParams.getFirst("query");
 		return OutputStreamHandler.getConverterStreamingOutput(new CSVConverter(),query );
@@ -29,10 +34,22 @@ public class ConverterRoute {
 	@GET
 	@Path("rdb-converter.sql")
 	@Produces({"application/sql"})
-	public StreamingOutput getRDBExample(@Context UriInfo uriInfo) {
+	public StreamingOutput getRDBConverter(@Context UriInfo uriInfo) {
 		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters(); 
 		String query = queryParams.getFirst("query");
 		return OutputStreamHandler.getConverterStreamingOutput(new RDBConverter(), query);
 
 	}
+	
+	
+	@GET
+	@Path("json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONOutput getJSONConverter(@Context UriInfo uriInfo){
+		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters(); 
+		String query = queryParams.getFirst("query");
+		JSONConverter converter = new JSONConverter(SPARQLHandler.executeDBPediaQuery(query));
+		return converter.jsonOutput;
+	}
+
 }
