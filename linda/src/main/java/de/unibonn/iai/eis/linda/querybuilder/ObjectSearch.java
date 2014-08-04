cc0f83144b83644d98637bf42440d529d2ec947e
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-08-04 19:25:05 +0200

    created ObjectSearch class for builder

---
+++ linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/ObjectSearch.java
@@ -0,0 +1,39 @@
+package de.unibonn.iai.eis.linda.querybuilder;
+
+import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
+
+public class ObjectSearch {
+
+	/**
+	 * @author gauravsingharoy
+	 * 
+	 * This class searches for objects of a class in an RDF dataset
+	 */
+	
+		public String dataset;
+		public String searchString;
+		public String[] classes;
+		public ObjectSearch(String dataset, String searchString, String classes){
+			this.dataset = dataset;
+			this.searchString = searchString;
+			this.classes = classes.split(",");
+		}
+		
+		public String getSPARQLQuery(){
+			String query = SPARQLHandler.getPrefixes();
+			query += " SELECT distinct ?object ?label ";
+			query += " WHERE { " ; 
+			for(int i = 0; i< this.classes.length;i++){
+				if(i>0){
+					query += " UNION ";
+				}
+				query += " {?object rdf:type <"+this.classes[i]+">} ";
+			}
+			query += ". ?object rdfs:label ?label.  ";
+			query += " FILTER(bound(?label) && langMatches(lang(?label), \"EN\") && REGEX(?label, \""+this.searchString+"\"))}";
+			return query;
+		}
+
+	
+
+}
