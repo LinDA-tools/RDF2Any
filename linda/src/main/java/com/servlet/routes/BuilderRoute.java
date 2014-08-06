1cbf242bd7c646fb3357da7bde92e4be88795eb2
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-08-06 19:08:27 +0200

    added route for searching of objects

--- linda/src/main/java/com/servlet/routes/BuilderRoute.java
+++ linda/src/main/java/com/servlet/routes/BuilderRoute.java
@@ -12,14 +12,15 @@ import de.unibonn.iai.eis.linda.converters.impl.JSONConverter;
 import de.unibonn.iai.eis.linda.converters.impl.results.JSONOutput;
 import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
 import de.unibonn.iai.eis.linda.querybuilder.ClassSearch;
+import de.unibonn.iai.eis.linda.querybuilder.ObjectSearch;
 
 @Path("/v1.0/builder/")
 public class BuilderRoute {
-	
+	//This route is for the free text search of classes
 	@GET
 	@Path("classes")
 	@Produces(MediaType.APPLICATION_JSON)
-	public JSONOutput getJSONConverter(@Context UriInfo uriInfo){
+	public JSONOutput getClasses(@Context UriInfo uriInfo){
 		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters(); 
 		String search = queryParams.getFirst("search");
 		String dataset = queryParams.getFirst("dataset");
@@ -29,6 +30,24 @@ public class BuilderRoute {
 		Double endMilliseconds = (double) System.currentTimeMillis( );
 		converter.jsonOutput.setTimeTaken((endMilliseconds-startMilliseconds)/1000);
 		System.out.println("FINISH searching for classes ... ");
+		return converter.jsonOutput;
+	}
+	
+	//This route is for the free text search of objects
+	@GET
+	@Path("objects")
+	@Produces(MediaType.APPLICATION_JSON)
+	public JSONOutput getObjects(@Context UriInfo uriInfo){
+		MultivaluedMap<String, String> queryParams = uriInfo.getQueryParameters(); 
+		String search = queryParams.getFirst("search");
+		String dataset = queryParams.getFirst("dataset");
+		String classes = queryParams.getFirst("classes");
+		System.out.println("START Searching for objects of classes '"+classes+"' matching '"+search+"' in dataset '"+dataset+"'");
+		Double startMilliseconds = (double) System.currentTimeMillis( );
+		JSONConverter converter = new JSONConverter(SPARQLHandler.executeDBPediaQuery(new ObjectSearch(dataset,search,classes).getSPARQLQuery()));
+		Double endMilliseconds = (double) System.currentTimeMillis( );
+		converter.jsonOutput.setTimeTaken((endMilliseconds-startMilliseconds)/1000);
+		System.out.println("FINISH searching for objects ... ");
 		return converter.jsonOutput;
 	}
 }
