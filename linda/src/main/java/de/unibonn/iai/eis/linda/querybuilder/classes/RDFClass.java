b789adefc0fd36d30a87f4b66cd6b6b7c6be2256
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-09-10 19:39:59 +0200

    method to return SPARQL query to get properties of a class

--- linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClass.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClass.java
@@ -2,6 +2,8 @@ package de.unibonn.iai.eis.linda.querybuilder.classes;
 
 import java.util.List;
 
+import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
+
 public class RDFClass {
 
 	/**
@@ -18,6 +20,18 @@ public class RDFClass {
 	//this method will generate properties for the object
 	public void generateProperties(){
 		
+	}
+	
+	//this method returns the query to get properties of a class
+	public String getPropertiesSPARQLQuery(String propertyType){
+		String query = SPARQLHandler.getPrefixes();
+		query += "SELECT DISTINCT ?property ?label WHERE { ?concept rdf:type <"+this.uri+">. ?concept ?property ?o. ?property rdfs:label ?label. ";
+		if(propertyType == "object" )
+			query += " ?property rdf:type owl:ObjectProperty. ?property rdfs:range ?range. ";
+		else if(propertyType == "datatype")
+			query += " ?property rdf:type owl:DatatypeProperty. ";
+		query += " FILTER(langMatches(lang(?label), 'EN'))} LIMIT 40";
+		return query;
 	}
 	public String toString(){
 		String result = "uri : "+this.uri+", dataset : "+this.dataset;
