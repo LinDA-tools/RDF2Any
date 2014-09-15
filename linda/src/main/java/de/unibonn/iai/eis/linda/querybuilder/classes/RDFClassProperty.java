3fddd3ce43abbd6894cf34ad95e8191c24599724
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-09-15 14:19:18 +0200

    added a variable multiplePropertiesForSameNode in RDFClassProperty

--- linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClassProperty.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClassProperty.java
@@ -2,7 +2,7 @@ package de.unibonn.iai.eis.linda.querybuilder.classes;
 
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.rdf.model.Literal;
+
 
 import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
 
@@ -13,12 +13,14 @@ public class RDFClassProperty {
 	public String label;
 	public Integer count;
 	public RDFClassPropertyRange range;
+	public Boolean multiplePropertiesForSameNode;
 	
 	public RDFClassProperty(String uri,  String type, String label){
 		this.uri = uri;
 		this.count = 0;
 		this.type = type;
 		this.label = label;
+		this.multiplePropertiesForSameNode = false;
 	}
 	
 	public RDFClassProperty(String uri,  String type, String label, RDFClassPropertyRange range){
@@ -27,17 +29,23 @@ public class RDFClassProperty {
 		this.type = type;
 		this.label = label;
 		this.range = range;
+		this.multiplePropertiesForSameNode = false;
 	}
 	
 	//This method generates the count of the property
 	
 	public void generateCountOfProperty(String classUri, String dataset){
 		String countQuery = SPARQLHandler.getPrefixes();
-		countQuery += " SELECT distinct ?c where {?c rdf:type <"+classUri+">. ?c <"+this.uri+"> ?d} ";
+		countQuery += " SELECT ?c ?d  where {?c rdf:type <"+classUri+">. ?c <"+this.uri+"> ?d} ";
 		ResultSet countResultSet = SPARQLHandler.executeQuery(dataset, countQuery);
 		this.count = 0;
+		String prevUri = "";
 		while(countResultSet.hasNext()){
-			countResultSet.next();
+			QuerySolution row = countResultSet.next();
+			String currUri = row.get("g").toString();
+			if(prevUri.equals(currUri))
+				this.multiplePropertiesForSameNode = true;
+			prevUri = currUri;
 			this.count++;
 		}
 		System.out.println("generated count for "+this.uri + " ("+this.count+")");
