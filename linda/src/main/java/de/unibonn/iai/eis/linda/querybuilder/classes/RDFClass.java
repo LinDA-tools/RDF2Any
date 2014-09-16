ded28a4d20b1eb9cd55e7e7b8dd36f2075a3a089
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-09-16 02:13:09 +0200

    added query to get properties from schema

--- linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClass.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClass.java
@@ -15,64 +15,80 @@ public class RDFClass {
 	/**
 	 * @author gauravsingharoy
 	 * 
-	 * This class will contain an RDF class and its properties
+	 *         This class will contain an RDF class and its properties
 	 * 
 	 */
 	public String uri;
 	public String dataset;
 	public List<RDFClassProperty> properties;
-	public RDFClass(String dataset,String uri){
+
+	public RDFClass(String dataset, String uri) {
 		this.uri = uri;
 		this.dataset = dataset;
 		this.properties = new ArrayList<RDFClassProperty>();
 	}
-	
-	//this method will generate properties for the object from SPARQL endpoint
-	public void generatePropertiesFromSPARQL(){
-		//Get dataType properties
-		ResultSet dataTypeProperties = SPARQLHandler.executeQuery(this.dataset,getPropertiesSPARQLQuery("datatype"));
-		addRdfResultSetToProperties(dataTypeProperties,"datatype");
-		//Get object properties
-		ResultSet objectProperties = SPARQLHandler.executeQuery(this.dataset,getPropertiesSPARQLQuery("object"));
-		addRdfResultSetToProperties(objectProperties,"object");
+
+	// this method will generate properties for the object from SPARQL endpoint
+	public void generatePropertiesFromSPARQL() {
+		// Get dataType properties
+		ResultSet dataTypeProperties = SPARQLHandler.executeQuery(this.dataset,
+				getPropertiesSPARQLQuery("datatype"));
+		addRdfResultSetToProperties(dataTypeProperties, "datatype");
+		// Get object properties
+		ResultSet objectProperties = SPARQLHandler.executeQuery(this.dataset,
+				getPropertiesSPARQLQuery("object"));
+		addRdfResultSetToProperties(objectProperties, "object");
 	}
-	public void addRdfResultSetToProperties(ResultSet resultSetProperties, String type){
-		addRdfResultSetToProperties( resultSetProperties,  type, false);
+
+	public void addRdfResultSetToProperties(ResultSet resultSetProperties,
+			String type) {
+		addRdfResultSetToProperties(resultSetProperties, type, false);
 	}
-	//This method adds the ResultSet properties to the properties List 
-	public void addRdfResultSetToProperties(ResultSet resultSetProperties, String type, Boolean doStatisticalQueries){
-		while(resultSetProperties.hasNext()){
+
+	// This method adds the ResultSet properties to the properties List
+	public void addRdfResultSetToProperties(ResultSet resultSetProperties,
+			String type, Boolean doStatisticalQueries) {
+		while (resultSetProperties.hasNext()) {
 			QuerySolution row = resultSetProperties.next();
 			RDFNode propertyNode = row.get("property");
 			Literal propertyLabel = (Literal) row.get("label");
-			RDFClassProperty p = new RDFClassProperty(propertyNode.toString(), type, SPARQLHandler.getLabelName(propertyLabel), new RDFClassPropertyRange("", ""));
-			if(doStatisticalQueries)
+			RDFClassProperty p = new RDFClassProperty(propertyNode.toString(),
+					type, SPARQLHandler.getLabelName(propertyLabel),
+					new RDFClassPropertyRange("", ""));
+			if (doStatisticalQueries)
 				p.generateCountOfProperty(this.uri, this.dataset);
 			properties.add(p);
-			
+
 		}
 	}
-	
-	//this method returns the query to get properties of a class
-	public String getPropertiesSPARQLQuery(String propertyType){
+
+	// this method returns the query to get properties of a class
+	public String getPropertiesSPARQLQuery(String propertyType) {
 		String query = SPARQLHandler.getPrefixes();
-		query += "SELECT DISTINCT ?property ?label WHERE { ?concept rdf:type <"+this.uri+">. ?concept ?property ?o. ?property rdfs:label ?label. ";
-		if(propertyType == "object" )
-			query += " ?property rdf:type owl:ObjectProperty. ?property rdfs:range ?range. ";
-		else if(propertyType == "datatype")
-			query += " ?property rdf:type owl:DatatypeProperty. ";
-		query += " FILTER(langMatches(lang(?label), 'EN'))} LIMIT 70";
+		if (propertyType.equals("schema")) {
+			query += "SELECT DISTINCT ?property ?label WHERE { ?property rdfs:domain <"+this.uri+">. ?property rdfs:range ?range.  ?property rdfs:label ?label.";
+		} else {
+			query += "SELECT DISTINCT ?property ?label WHERE { ?concept rdf:type <"
+					+ this.uri
+					+ ">. ?concept ?property ?o. ?property rdfs:label ?label. ";
+
+			if (propertyType.equals("object"))
+				query += " ?property rdf:type owl:ObjectProperty. ?property rdfs:range ?range. ";
+			else if (propertyType.equals("datatype"))
+				query += " ?property rdf:type owl:DatatypeProperty. ";
+		}
+		query += " FILTER(langMatches(lang(?label), 'EN'))} LIMIT 50";
 		return query;
 	}
-	public String toString(){
-		String result = "uri : "+this.uri+", dataset : "+this.dataset;
-		for(Integer i=0;i<properties.size();i++){
+
+	public String toString() {
+		String result = "uri : " + this.uri + ", dataset : " + this.dataset;
+		for (Integer i = 0; i < properties.size(); i++) {
 			result += "\n" + properties.get(i).toString();
 		}
-		
+
 		return result;
-		
+
 	}
-	
 
 }
