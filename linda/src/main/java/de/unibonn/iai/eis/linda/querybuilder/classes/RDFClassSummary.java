77a6209441d235e6263fc07403f7e0170b2a414e
Gaurav SinghaRoy <gaurav@tiatros.com>
2014-11-10 15:39:26 +0100

    implemented method to generate count of class objects

--- linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClassSummary.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClassSummary.java
@@ -3,13 +3,18 @@ package de.unibonn.iai.eis.linda.querybuilder.classes;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+
 import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
 import de.unibonn.iai.eis.linda.querybuilder.objects.RDFObject;
 
 /**
  * @author gauravsingharoy
  * 
- *         This class will contain an RDF class and some summary items, viz, total objects and some object examples
+ *         This class will contain an RDF class and some summary items, viz,
+ *         total objects and some object examples
  * 
  */
 public class RDFClassSummary {
@@ -19,28 +24,44 @@ public class RDFClassSummary {
 	public String name;
 	public Integer total_objects;
 	public List<RDFObject> sample_objects;
-	
-	public RDFClassSummary(String dataset, String uri){
+
+	public RDFClassSummary(String dataset, String uri) {
 		this.uri = uri;
 		this.name = SPARQLHandler.getLabelFromNode(dataset, uri, "EN");
 		this.dataset = dataset;
 		this.total_objects = null;
 		this.sample_objects = new ArrayList<RDFObject>();
 	}
-	
-	public void generateSummaryItems(){
+
+	public void generateSummaryItems() {
 		generateTotalObjectsCount();
 		generateSampleObjects();
 	}
-	
-	public void generateTotalObjectsCount(){
+
+	public void generateTotalObjectsCount() {
 		this.total_objects = 0;
+		try {
+			String countQuery = SPARQLHandler.getPrefixes();
+			countQuery += " SELECT  (count(DISTINCT ?c) AS ?totalcount)  where {?c rdf:type <"
+					+ this.uri + ">. } ";
+			ResultSet countResultSet = SPARQLHandler.executeQuery(dataset,
+					countQuery);
+			if (countResultSet.hasNext()) {
+				QuerySolution row = countResultSet.next();
+				this.total_objects = SPARQLHandler.getIntegerValueOfLiteral(row
+						.get("totalcount"));
+			}
+		} catch (Exception e) {
+			System.out.println("Error in generating total objects count for "
+					+ this.uri + " : " + e.toString());
+		}
 	}
-	
-	public void generateSampleObjects(){
+
+	public void generateSampleObjects() {
 		generateSampleObjects(5);
 	}
-	public void generateSampleObjects(Integer limit){
-		
+
+	public void generateSampleObjects(Integer limit) {
+
 	}
 }
