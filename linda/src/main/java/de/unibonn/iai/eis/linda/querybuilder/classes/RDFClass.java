a14e5bac9b3c70e117baf14cbc9702a2fe9f7ff3
judie90 <judieattard@cinnamon.local>
2016-04-08 13:42:28 +0200

    fixed link for indexing

--- linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClass.java
+++ linda/src/main/java/de/unibonn/iai/eis/linda/querybuilder/classes/RDFClass.java
@@ -2,6 +2,7 @@ package de.unibonn.iai.eis.linda.querybuilder.classes;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -11,9 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.lang3.text.WordUtils;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFLanguages;
+import org.apache.http.HttpException;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -23,36 +22,26 @@ import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.SimpleFSDirectory;
-import org.apache.lucene.util.Version;
-import org.openjena.riot.Lang;
 
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.rdf.model.ModelFactory;
-import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.owlike.genson.annotation.JsonIgnore;
 
 import de.unibonn.iai.eis.linda.helper.CommonHelper;
 import de.unibonn.iai.eis.linda.helper.LuceneHelper;
-import de.unibonn.iai.eis.linda.helper.RDBHelper;
 import de.unibonn.iai.eis.linda.helper.SPARQLHandler;
 
 public class RDFClass {
@@ -67,12 +56,14 @@ public class RDFClass {
 	public String dataset;
 	public String label;
 	public List<RDFClassProperty> properties;
-
+	public String status;
+	
 	public RDFClass(String dataset, String uri) {
 		this.uri = uri;
-		this.label = SPARQLHandler.getLabelFromNode(dataset, uri, "EN");					
+		this.label = SPARQLHandler.getLabelFromNode(dataset, uri, "EN");
 		this.dataset = dataset;
 		this.properties = new ArrayList<RDFClassProperty>();
+		this.status = "";
 	}
 
 	public RDFClass(String dataset, String uri, String label) {
@@ -80,100 +71,143 @@ public class RDFClass {
 		this.label = label;
 		this.dataset = dataset;
 		this.properties = new ArrayList<RDFClassProperty>();
+		this.status = "";
 	}
 
 	// this method will generate properties for the object from SPARQL endpoint
 
-	public void generatePropertiesFromSPARQL() {
-		generatePropertiesFromSPARQL(false);
-	}
-
 	public void generatePropertiesFromSPARQL(Boolean doStatisticalQueries) {
 		// Get dataType properties
-		ResultSet dataTypeProperties = SPARQLHandler.executeQuery(this.dataset,
-				getPropertiesSPARQLQuery("datatype", this.dataset));
-		addRdfResultSetToProperties(dataTypeProperties, "datatype",
-				doStatisticalQueries);
+		try{
+			ResultSet dataTypeProperties = SPARQLHandler.executeQuery(this.dataset, getPropertiesInformationAndCountSPARQLQuery("DatatypeProperty"));
+			addRdfResultSetToProperties(dataTypeProperties, "data",doStatisticalQueries);
+			this.status = "Success";
+		} catch (Exception e){
+			try{
+				ResultSet dataTypeProperties = SPARQLHandler.executeQuery(this.dataset, getPropertiesSPARQLQuery("DatatypeProperty"));
+				addPropertyInformationAndCount(dataTypeProperties, "data");
+				this.status = "Success";
+			} catch (Exception e1){
+				this.status = "Error retrieving data from "+this.dataset;
+			}
+		}
 		// Get object properties
-		ResultSet objectProperties = SPARQLHandler.executeQuery(this.dataset,
-				getPropertiesSPARQLQuery("object", this.dataset));
-		addRdfResultSetToProperties(objectProperties, "object",
-				doStatisticalQueries);
-		ResultSet schemaProperties = SPARQLHandler.executeQuery(this.dataset,
-				getPropertiesSPARQLQuery("schema", this.dataset));
-		addRdfResultSetToProperties(schemaProperties, "schema",
-				doStatisticalQueries);
-	}
-
-	public void addRdfResultSetToProperties(ResultSet resultSetProperties,
-			String type) {
-		addRdfResultSetToProperties(resultSetProperties, type, false);
+		try{
+			ResultSet objectProperties = SPARQLHandler.executeQuery(this.dataset,getPropertiesInformationAndCountSPARQLQuery("ObjectProperty"));
+			addRdfResultSetToProperties(objectProperties, "object",doStatisticalQueries);
+			this.status = "Success";
+		} catch (Exception e){
+			try{
+				ResultSet objectProperties = SPARQLHandler.executeQuery(this.dataset, getPropertiesSPARQLQuery("ObjectProperty"));
+				addPropertyInformationAndCount(objectProperties, "object");
+				this.status = "Success";
+			} catch (Exception e1){
+				this.status = "Error retrieving data from "+this.dataset;
+			}
+		}
 	}
 
 	// This method adds the ResultSet properties to the properties List
-	public void addRdfResultSetToProperties(ResultSet resultSetProperties,
-			String type, Boolean doStatisticalQueries) {
+	public void addRdfResultSetToProperties(ResultSet resultSetProperties, String type, Boolean doStatisticalQueries) {
 		while (resultSetProperties.hasNext()) {
 			QuerySolution row = resultSetProperties.next();
-			RDFNode propertyNode = row.get("property");
-			Literal propertyLabel = (Literal) row.get("label");
-			String propertyNodeUri = propertyNode.toString();
-			if (!type.equalsIgnoreCase("schema")
-					|| (type.equalsIgnoreCase("schema") && !isPropertyPresent(propertyNodeUri))) {
-				RDFClassProperty p = new RDFClassProperty(propertyNodeUri,
-						type, SPARQLHandler.getLabelName(propertyLabel),
-						new RDFClassPropertyRange("", ""));
-				if (doStatisticalQueries)
-					p.generateCountOfProperty(this.uri, this.dataset);
-				p.generateRange(this.dataset);
+			Resource propertyNode = row.get("property").asResource();
+			Literal propertyLabel = row.getLiteral("label");
+			String propertyNodeUri = propertyNode.getURI();
+			Resource range = (row.get("range") == null) ? null : row.get("range").asResource();
+			Literal rangeLabel = (row.get("rangeLabel") == null) ? null : row.getLiteral("rangeLabel");
+			
+			if (!isPropertyPresent(propertyNodeUri)) {
+				RDFClassProperty p = new RDFClassProperty(propertyNodeUri, type, propertyLabel.getString(), new RDFClassPropertyRange("", ""));
+				if (doStatisticalQueries) p.addCountOfProperty(row.get("cnt").asLiteral().getInt());
+				p.addRange(range, rangeLabel, type);
+				p.addHintExample(this.dataset);
 				properties.add(p);
 			}
 
 		}
 	}
 
-	// this method returns the query to get properties of a class
-	@JsonIgnore
-	public String getPropertiesSPARQLQuery(String propertyType, String dataset) {
-		return getPropertiesSPARQLQuery(propertyType, 25, dataset);
-	}
-
-	@JsonIgnore
-	public String getPropertiesSPARQLQuery(String propertyType, Integer limit, String dataset) {
-		String query = SPARQLHandler.getPrefixes();
-		if (propertyType.equals("schema")) {
-			query += "SELECT DISTINCT ?property ?label WHERE { ?property rdfs:domain <"
-					+ this.uri
-					+ ">. ?property rdfs:range ?range.  ?property rdfs:label ?label.";
-		} else {
+	
+	public void addPropertyInformationAndCount(ResultSet resultSetProperties, String type){
+		while (resultSetProperties.hasNext()) {
+			QuerySolution row = resultSetProperties.next();
+			Resource propertyNode = row.get("property").asResource();
+			Literal propertyLabel = row.getLiteral("label");
+			String propertyNodeUri = propertyNode.getURI();
 			
-			if (dataset.contains("dbpedia")){
-				query += "SELECT DISTINCT ?property ?label WHERE {";
-				if (propertyType.equals("object"))
-					query += " ?property rdf:type owl:ObjectProperty";
-				else if (propertyType.equals("datatype"))	
-					query += " ?property rdf:type owl:DatatypeProperty. ";
-				query += "{ SELECT DISTINCT ?superClass { <"+
-					this.uri + "> rdfs:subClassOf ?superClass ." +
-					"} } { " +
-					"?property rdfs:domain <"+this.uri+"> . }" +
-					"UNION { ?property rdfs:domain ?superClass . }   ?property rdfs:label ?label . ";
-			}
-			else{
-				query += "SELECT DISTINCT ?property ?label WHERE { ?concept rdf:type <"
-				+ this.uri
-				+ ">. ?concept ?property ?o. ?property rdfs:label ?label. ";
-
-		if (propertyType.equals("object"))
-			query += " ?property rdf:type owl:ObjectProperty. ?property rdfs:range ?range. ";
-		else if (propertyType.equals("datatype"))
-			query += " ?property rdf:type owl:DatatypeProperty. ";
-		
+			if (!isPropertyPresent(propertyNodeUri)) {
+				ResultSet info = SPARQLHandler.executeQuery(this.dataset, getInformationAndCountSPARQLQuery(propertyNodeUri));
+				
+				Resource range = null;
+				Literal rangeLabel = null;
+				Literal count = null;
+				while(info.hasNext()){
+					QuerySolution infoRow = info.next();
+					range = (infoRow.get("range") == null) ? null : infoRow.get("range").asResource();
+					rangeLabel = (infoRow.get("rangeLabel") == null) ? null : infoRow.getLiteral("rangeLabel");
+					count = (infoRow.get("cnt") == null) ? null : infoRow.getLiteral("cnt");
+				}
+				
+				RDFClassProperty p = new RDFClassProperty(propertyNodeUri, type, propertyLabel.getString(), new RDFClassPropertyRange("", ""));
+				p.addCountOfProperty(count.getInt());
+				p.addRange(range, rangeLabel, type);
+				p.addHintExample(this.dataset);
+				properties.add(p);
 			}
 		}
-		query += " FILTER(langMatches(lang(?label), 'EN'))} ";
-//		if (!propertyType.equalsIgnoreCase("schema"))
-//			query += "LIMIT " + limit.toString();
+	}
+	
+	
+	
+	@JsonIgnore
+	public String getPropertiesInformationAndCountSPARQLQuery(String propertyType) {
+		String query = "";
+		URL url = Resources.getResource("builder_queries/GetPropertiesInformationAndCount.sparql");
+			
+		try {
+			query = Resources.toString(url, Charsets.UTF_8);
+			query = query.replace("%%Concept-URI%%", this.uri);
+			query = query.replace("%%Type%%", propertyType);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		System.out.println(query);
+		return query;
+	}
+	
+	@JsonIgnore
+	public String getPropertiesSPARQLQuery(String propertyType) {
+		String query = "";
+		URL url = Resources.getResource("builder_queries/GetProperties.sparql");
+			
+		try {
+			query = Resources.toString(url, Charsets.UTF_8);
+			query = query.replace("%%Concept-URI%%", this.uri);
+			query = query.replace("%%Type%%", propertyType);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		System.out.println(query);
+		return query;
+	}
+	
+	@JsonIgnore
+	public String getInformationAndCountSPARQLQuery(String propertyURI) {
+		String query = "";
+		URL url = Resources.getResource("builder_queries/PropertyInformationAndCount.sparql");
+			
+		try {
+			query = Resources.toString(url, Charsets.UTF_8);
+			query = query.replace("%%Concept-URI%%", this.uri);
+			query = query.replace("%%Property-URI%%", propertyURI);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		System.out.println(query);
 		return query;
 	}
 
@@ -200,13 +234,12 @@ public class RDFClass {
 			}
 		}
 	}
-
+	
 	// this method creates indexes in lucene for the properties
 	@SuppressWarnings("deprecation")
 	public void generateLuceneIndexes() throws IOException {
 		deleteIndexes();
-		System.out.println("Creating indexes for class .. " + this.label + " <"
-				+ this.uri + ">");
+		System.out.println("Creating indexes for class .. " + this.label + " <"+ this.uri + ">");
 		
 		for (RDFClassProperty property : this.properties) {
 			addLucenePropertyDoc(property);
@@ -262,6 +295,9 @@ public class RDFClass {
 		d.add(new StringField("range_uri", property.range.uri, Field.Store.YES));
 		d.add(new StringField("range_label", property.range.label,
 				Field.Store.YES));
+		d.add(new StringField("hintExample",
+				property.hintExample.toString(),
+				Field.Store.YES));
 		w.addDocument(d);
 
 		w.close();
@@ -270,8 +306,7 @@ public class RDFClass {
 
 	// this method searches for a matching class
 
-	public static RDFClass searchRDFClass(String dataset, String classUri)
-			throws ParseException {
+	public static RDFClass searchRDFClass(String dataset, String classUri) throws ParseException {
 		return searchRDFClass(dataset, classUri, true);
 	}
 	
@@ -290,7 +325,6 @@ public class RDFClass {
 	}
 	
 
-	@SuppressWarnings("deprecation")
 	public static RDFClass searchRDFClass(String dataset, String classUri,
 			Boolean forceIndexCreation) throws ParseException {
 		RDFClass resultClass = new RDFClass(dataset, classUri);
@@ -788,7 +822,7 @@ public class RDFClass {
 		List<String> failedClasses = new ArrayList<String>();
 		classesQuery += " select distinct ?class where {?class rdf:type owl:Class. ?class rdfs:label ?label. FILTER(langMatches(lang(?label), \"en\"))}";
 	
-		ResultSet classesResultSet = SPARQLHandler.executeQuery("http://dbpedia.org/sparql",classesQuery);
+		ResultSet classesResultSet = SPARQLHandler.executeQuery("http://live.dbpedia.org/sparql",classesQuery);
 		
 		Integer classCounter = 0;
 
@@ -822,7 +856,7 @@ public class RDFClass {
 	
 	public static RDFClass reindexLocalDBPediaRDFClass(String classUri) throws ParseException {
 		System.out.println("Reindexing for " + classUri + ". Will create indexes now ..." );
-		RDFClass resultClass = new RDFClass("http://dbpedia.org/sparql", classUri);
+		RDFClass resultClass = new RDFClass("http://live.dbpedia.org/sparql", classUri);
 		resultClass.generatePropertiesFromSPARQL(true);
 		try {
 				resultClass.generateLuceneIndexes();
